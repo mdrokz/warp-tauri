81f07a952000d6fe8be087aee71f4226fcf22eed
mdrokz <mohammadmunshi@gmail.com>
2022-11-07 18:54:11 +0530

    add file to store constants like url & regex

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const WORMHOLE_DEFAULT_RENDEZVOUS_SERVER = "ws://relay.magic-wormhole.io:4000"
+export const WORMHOLE_DEFAULT_TRANSIT_RELAY = "tcp://transit.magic-wormhole.io:4001";
+export const WORMHOLE_DEFAULT_APPID_STR = "lothar.com/wormhole/text-or-file-xfer";
+
+export const TRANSMIT_URI_FIND_REGEX = /wormhole-transfer:\d+-\S+/;
+export const TRANSMIT_CODE_FIND_REGEX = /\d+-\S+/;
+export const TRANSMIT_CODE_MATCH_REGEX = /^\d+-\S+$/;
+
